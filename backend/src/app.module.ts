19d2b8427035ca2190c7ce82437dd0e149c9c234
Ho Phuoc Hoan <phuochoan17032004@gmail.com>
2025-11-25 17:54:30 +0700

    refactor: remove unused AppController and AppService from the application

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { User } from './users/entities/user.entity';
@@ -64,7 +62,5 @@ import { DashboardModule } from './dashboard/dashboard.module';
     QuizzesModule,
     DashboardModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
